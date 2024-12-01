6d6691e2d94e0ff3406afe441d45fecfe77655cd
Francisco Franco <servigramco@gmail.com>
2024-12-01 17:17:22 -0500

    agregando requirements backend

--- task_frontend/src/pages/Tasks/tasks.tsx
+++ task_frontend/src/pages/Tasks/tasks.tsx
@@ -1,50 +1,11 @@
 import React from 'react'
-import { useTable, Column } from "react-table"
+import { useTable } from "react-table"
 
-interface TableProps<T extends object> {
-    columns: Column<T>[];
-    data: T[];
-}
-
-const Tasks = <T extends object>({ columns, data }: TableProps<T>) => {
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable<T>({ columns, data });
+const Tasks: React.FC = () => {
 
     return (
-        <table {...getTableProps()} style={{ borderCollapse: "collapse", width: "100%" }}>
-            <thead>
-                {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                            <th {...column.getHeaderProps()} style={{ border: "1px solid black", padding: "8px" }}>
-                                {column.render("Header")}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map((cell) => (
-                                <td {...cell.getCellProps()} style={{ border: "1px solid black", padding: "8px" }}>
-                                    {cell.render("Cell")}
-                                </td>
-                            ))}
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-    );
-};
-
+        <div>tasks</div>
+    )
+}
 
 export default Tasks
